d0fa19c0b97f1f1fd45b1c189661e4407a1df587
Micky Socaci <micky@nowlive.ro>
2018-12-16 11:41:06 +0200

    Modman and modules

--- .modman/Amasty_Customerattr/src/app/code/local/Amasty/Customerattr/Block/Adminhtml/Order/Create/Form/Account/Orderattr.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * @author Amasty Team
- * @copyright Copyright (c) 2016 Amasty (https://www.amasty.com)
- * @package Amasty_Customerattr
- */
-
-
-class Amasty_Customerattr_Block_Adminhtml_Order_Create_Form_Account_Pure extends Amasty_Orderattr_Block_Adminhtml_Order_Create_Form_Account{}
